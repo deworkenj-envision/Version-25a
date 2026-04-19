66e702778c00665892165d924b2c8c6584e41e94
deworkenj-envision <deworkenj@gmail.com>
2026-04-19 15:38:58 -0700

    Update page.js

--- app/admin/orders/page.js
+++ app/admin/orders/page.js
@@ -625,7 +625,7 @@ export default function AdminOrdersPage() {
                           rel="noreferrer"
                           className="mt-2 inline-flex items-center justify-center rounded-xl bg-slate-900 px-4 py-2 text-sm font-semibold text-white hover:bg-slate-800"
                         >
-                          Open Sheet
+                          Print Packing Slip
                         </a>
                       </div>
                     </div>
