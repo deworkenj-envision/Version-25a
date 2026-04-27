fdc09fa6fcc71a41ff4f99499d1c23707d79def8
deworkenj-envision <deworkenj@gmail.com>
2026-04-27 16:57:41 -0700

    updates

--- app/track/[token]/page.js
+++ app/track/[token]/page.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { supabaseAdmin } from "@/lib/supabaseAdmin";
+import { supabaseAdmin } from "../../../lib/supabaseAdmin";
 
 export const dynamic = "force-dynamic";
 
